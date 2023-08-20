2fc1d21670b3c0a9031b0a00d8a2f9203f86828a
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-08-20 19:29:44 +0300

    fixed script for the github repo

--- index.php
+++ index.php
@@ -217,6 +217,7 @@ foreach ($data as $key => $value) {
   <meta property="og:description" content="Check the status of various servers. This page displays the online status of servers, including scylla.cs.uoi.gr, ecourse.uoi.gr, eudoxus.gr, and classweb.uoi.gr.">
   <meta property="og:type" content="website">
   <meta property="og:url" content="https://cse.uoi.gr/~cse74134/servers">
+  <script src="https://tarptaeya.github.io/repo-card/repo-card.js"></script>
   <!-- Add Twitter image here using: <meta name="twitter:image" content="your-image-url.jpg"> -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
@@ -313,6 +314,5 @@ foreach ($data as $key => $value) {
   });
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-<script src="https://tarptaeya.github.io/repo-card/repo-cardbadge bg-success.js"></script>
 </body>
 </html>
